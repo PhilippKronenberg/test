b7da5dc526421a3763ef304c3822da0f620eb809
PhilippKronenberg <72253537+PhilippKronenberg@users.noreply.github.com>
2020-11-20 11:34:07 +0100

    Update demo_gha.R

--- solutions/demo_gha.R
+++ solutions/demo_gha.R
@@ -1,29 +1,19 @@
 #!/usr/bin/env Rscript
 
 library(data.table)
-
 library(tsbox)
-
 library(zoo)
-
 library(OECD)
 
 small <- get_dataset("STLABOUR", filter = list("CHE","LRHUTTTT"))
 
-
-
 dt_l <- data.table(small)
 
 tsdt <- dt_l[
-  
   SUBJECT == "LRHUTTTT" & FREQUENCY == "Q",
-  
-  list(id = tolower(paste(LOCATION,SUBJECT,MEASURE,sep=".")),
-       
+  list(id = tolower(paste(LOCATION,SUBJECT,MEASURE,sep=".")), 
        time = as.Date(as.yearqtr(gsub("-"," ",obsTime))),
-       
        value = obsValue)
-  
 ]
 
 
